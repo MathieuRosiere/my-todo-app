77aae13fb70a2ba806297036f8df8a30e1cbffa7
MathieuRosiere <rosiere.mathieu@gmail.com>
2024-05-30 13:18:50 +0200

    chore(lint-staged): installation & config

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,8 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...pluginVue.configs["flat/recommended"],
 ];
