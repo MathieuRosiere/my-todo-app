deb1dc717ee55c033c3f07e391750ab228b024b0
MathieuRosiere <rosiere.mathieu@gmail.com>
2024-06-12 11:32:26 +0200

    build(api): endpoints for table Todos

--- schemas/todo.js
+++ schemas/todo.js
@@ -1,11 +1,22 @@
 import { z } from "zod";
 
-export const todoInput = z.object({
-    title: z.string().int(),
+export const todoCreateInput = z.object({
+    title: z.string().trim(),
+    userId: z.number().int(),
 });
 
-export const todoOutput = z.object({
-    id: z.number().int(),
-    title: z.string(),
-    completed: z.boolean(),
-});
+export const todoOutput = z
+    .object({
+        id: z.number().int(),
+        title: z.string(),
+        completed: z.boolean(),
+    })
+    .strict();
+
+export const todoUpdate = z
+    .object({
+        title: z.string(),
+        userId: z.number().int(),
+        completed: z.boolean(),
+    })
+    .strict();
---
+++ server/api/todos/index.get.js
@@ -0,0 +1,32 @@
+import { Todo } from "@/server/models/todo";
+import { todoOutput } from "@/schemas/todo";
+import { response } from "@/utils/response";
+
+// eslint-disable-next-line no-undef
+export default defineEventHandler(async () => {
+    try {
+        const todos = await Todo.findAll({
+            where: {
+                userId: 1,
+            },
+        });
+        const todoArray = [];
+        for (const item of todos) {
+            const todo = {
+                id: item.id,
+                title: item.title,
+                completed: item.completed,
+            };
+
+            const outputResult = todoOutput.safeParse(todo);
+            if (outputResult.success === false) {
+                return response.error(outputResult.error.issues);
+            } else {
+                todoArray.push(outputResult.data);
+            }
+        }
+        return response.succes(todoArray);
+    } catch (error) {
+        return response.error(error.message);
+    }
+});
