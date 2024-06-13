4f18657e261e25fc17911bbc24aea725e3e5bf89
MathieuRosiere <rosiere.mathieu@gmail.com>
2024-06-13 13:44:38 +0200

    chore: add a fake user in store for testing

--- stores/userStore.js
+++ stores/userStore.js
@@ -2,7 +2,11 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useUserStore = defineStore("userStore", () => {
-    const user = ref("");
+    const user = ref({
+        id: 1,
+        username: "John",
+        email: "john@test.com",
+    });
     const todos = ref([
         "learn CSS",
         "learn Javascript",
