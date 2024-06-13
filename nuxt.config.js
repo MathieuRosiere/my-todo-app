32726929efabad48cd009301e77a819f6c17519d
MathieuRosiere <rosiere.mathieu@gmail.com>
2024-06-13 13:10:31 +0200

    feat(vue-query): install package

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,4 +4,5 @@ export default defineNuxtConfig({
     css: ["~/assets/css/main.css", "@fortawesome/fontawesome-svg-core/styles.css"],
     modules: ["@pinia/nuxt", "@nuxt/eslint"],
     devtools: { enabled: true },
+    ssr: false,
 });
